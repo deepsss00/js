add97d578e0e9b5ac283e2865f9ca730e323c892
deepsss00 <109897947+deepsss00@users.noreply.github.com>
2023-09-05 15:15:24 +0000

    conversion of data types

--- .devcontainer/basicss/text.js
+++ .devcontainer/basicss/text.js
@@ -1,1 +1,15 @@
 console.log("Deepanjali")
+let score  = "33abc"
+console.log(typeof score);
+
+let name1 = Number(score)
+console.log(typeof name1 );
+
+// "33"=33
+//"33abc"=NaN
+// true=1; false=0
+
+let isLoggedin=1
+let isboolean= Boolean(isLoggedin)
+console.log(isboolean);
+//""=false
